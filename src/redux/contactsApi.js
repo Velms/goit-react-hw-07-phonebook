0ab0f927a9563af64cd7d387e67041d0354a010e
Velms <78916991+Velms@users.noreply.github.com>
2024-01-02 16:25:58 +0200

    Update contactsApi.js

--- src/redux/contactsApi.js
+++ src/redux/contactsApi.js
@@ -10,6 +10,12 @@ export const contactsApi = createApi({
     getContactsApi: builder.query({
       query: () => `contacts`,
       providesTags: result =>
+        result
+          ? [
+              ...result.map(({ id }) => ({ type: 'Contacts', id })),
+              { type: 'Contacts', id: 'LIST' },
+            ]
+          : [{ type: 'Contacts', id: 'LIST' }],
     }),
     addContact: builder.mutation({
       query: body => ({
