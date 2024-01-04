589fdc8089d97c94b66fb074360aeff274378747
Velms <78916991+Velms@users.noreply.github.com>
2024-01-04 16:55:09 +0200

    Update selectors.js

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,1 +1,10 @@
 export const selectFilter = state => state.filter.value;
+
+export const selectFilteredContacts = createSelector(
+    [selectContacts, selectFilter],
+    (contacts, filter) => {
+      return contacts.filter(({ name }) =>
+        name.toLowerCase().includes(filter.toLowerCase())
+      );
+    }
+  );
