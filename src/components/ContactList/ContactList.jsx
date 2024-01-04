012e198280c5f2e40c7697fd67d10227db1c378b
Velms <78916991+Velms@users.noreply.github.com>
2024-01-04 17:20:38 +0200

    fix

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -5,7 +5,7 @@ import { useGetContactsApiQuery } from 'redux/contactsApi';
 
 const ContactList = () => {
   const { data, isLoading } = useGetContactsApiQuery();
-  const filter = useSelector(selectFilter);
+  const filter = useSelector(state => state.filter.value);
 
   console.log(data);
 
